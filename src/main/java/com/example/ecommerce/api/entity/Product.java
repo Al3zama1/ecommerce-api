4450084dcf62bc3e99069e28b892b0e16d825df7
Abran Lezama <lezama.abran@gmail.com>
2023-02-09 15:45:29 -0800

    Product Entity
    - Added product entity with all database constraints

---
+++ src/main/java/com/example/ecommerce/api/entity/Product.java
@@ -0,0 +1,26 @@
+package com.example.ecommerce.api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false, length = 50, updatable = false)
+    private String name;
+    private String description;
+    @Column(name = "imageUrl")
+    private String imageUrl;
+    @Column(nullable = false)
+    private double price;
+    @Column(name = "stock_quantity", nullable = false)
+    private long stockQuantity;
+}
